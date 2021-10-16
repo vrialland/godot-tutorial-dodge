e4a2f9f2c221177e223264af1164aa14e115858d
Vincent Rialland <vincent.rialland@gmail.com>
2021-10-16 22:44:55 +0200

    Working prototype with HUD

---
+++ scripts/HUD.gd
@@ -0,0 +1,32 @@
+extends CanvasLayer
+
+signal start_game
+
+
+func show_message(text: String) -> void:
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+	
+func show_game_over() -> void:
+	show_message("Game over")
+	yield($MessageTimer, "timeout")
+	
+	$Message.text = "Dodge the \ncreeps!"
+	$Message.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$StartButton.show()
+	
+	
+func update_score(score: int) -> void:
+	$ScoreLabel.text = str(score)
+
+
+func _on_MessageTimer_timeout() -> void:
+	$Message.hide()
+
+
+func _on_StartButton_pressed() -> void:
+	$StartButton.hide()
+	emit_signal("start_game")
---
+++ scripts/Main.gd
@@ -0,0 +1,47 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+
+func _ready() -> void:
+	randomize()
+
+
+func game_over() -> void:
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	
+	
+func new_game() -> void:
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get ready!")
+
+
+func _on_MobTimer_timeout() -> void:
+	$MobPath/MobSpawnLocation.offset = randi()
+	var mob = Mob.instance()
+	add_child(mob)
+
+	var direction: float = $MobPath/MobSpawnLocation.rotation + PI / 2
+	mob.position = $MobPath/MobSpawnLocation.position
+	
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	
+
+func _on_ScoreTimer_timeout() -> void:
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_StartTimer_timeout() -> void:
+	$MobTimer.start()
+	$ScoreTimer.start()
